d3d5eb78cd4c629876493f835962df0ef539bd24
raulyfs11 <rauljulio9823@gmail.com>
2023-03-14 05:29:52 +0000

    new

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,7 @@ import "../style/index.css";
 /**
  *  EDIT ONLY INSIDE THIS RENDER FUNCTION
  *  This function is called every time the user changes types or changes any input
- * 
+ 
     {
         includeCover: true, // if includeCover is true the algorithm should
         background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the url of the image that will used as background for the profile cover
@@ -26,21 +26,57 @@ function render(variables = {}) {
   console.log("These are the current variables: ", variables); //print on the console
   // here we ask the logical questions to make decisions on how to build the html
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
+
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
 
+  // social media position
+
+  //social media
+  if (variables.twitter !== null) variables.twitter;
+  if (variables.github !== null) variables.github;
+  if (variables.linkedin !== null) variables.linkedin;
+  if (variables.instagram !== null) variables.instagram;
+
+  //name & last name
+  if (variables.name == null) variables.name = "Your name";
+  if (variables.lastname == null) variables.lastname = " & Last name";
+
+  //role
+  if (variables.role == null) variables.role = "<h2> Your role</h2>";
+  else if (variables.role == "Floor Planner")
+    variables.role = "<h2>Floor Planner</h2>";
+  else if (variables.role == "Thecnical Writer")
+    variables.role = "<h2> Technical Writer</h2>";
+  else if (variables.role == "Web Developer")
+    variables.role = "<h2> Web Developer</h2>";
+
+  // city
+  if (variables.city == null) variables.city = "Your city";
+  else if (variables.city == "Miami") variables.city = "Miami";
+  else if (variables.city == "Munich") variables.city = "Munich";
+  else if (variables.city == "Caracas") variables.city = "Caracas";
+  else if (variables.city == "Toronto") variables.city = "Toronto";
+
+  // country
+  if (variables.country == null) variables.country = "Your country";
+  else if (variables.country == "USA") variables.country = "USA";
+  else if (variables.country == "Germany") variables.country = "Germany";
+  else if (variables.country == "Canada") variables.country = "Canada";
+  else if (variables.country == "Venezuela") variables.country = "Venezuela";
+
   // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
+          ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name}  ${variables.lastname}</h1>
+          <h2>${variables.role}</h2>
+          <h3>${variables.city} , ${variables.country}</h3>
+          <ul class=${variables.socialMediaPosition}>
+            <li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -54,9 +90,10 @@ window.onload = function() {
     // if includeCover is true the algorithm should
     includeCover: true,
     // this is the url of the image that will used as background for the profile cover
-    background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
+    background:
+      "https://t4.ftcdn.net/jpg/02/69/82/11/360_F_269821180_UAEWi4xE7JhAqOUvOD1JoBLP0YDvqFvA.jpg",
     // this is the url for the profile avatar
-    avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
+    avatarURL: "https://cdn.onlinewebfonts.com/svg/img_569204.png",
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
